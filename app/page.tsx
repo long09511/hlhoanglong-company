73d26a4b7b83b6a07207eb453f685beaa981fced
long09511 <danglongpct2009@gmail.com>
2026-04-08 00:52:23 -0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <iframe
+      src="/index.html"
+      style={{ width: '100%', height: '100vh', border: 'none' }}
+      title="HL Hoang Long"
+    />
+  )
+}
+ 
